38205c1cd33e580e736ff32ee90141328bb50679
satyam-san <85273313+satyam-san@users.noreply.github.com>
2023-05-09 16:54:58 +0530

    Update spiral_print.cpp

--- spiral_print.cpp
+++ spiral_print.cpp
@@ -6,9 +6,43 @@ using namespace std;
 
 
 int main() {
-    int n = 4;
-    int m = 4;
-
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    vector <vector <int> > matrix(n, vector <int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    int sr = 0;
+    int sc = 0;
+    int ec = m - 1;
+    int er = n - 1;
+    
+    while (sr <= er && sc && ec) {
+        for (int i = sc; i <= ec; i++) {
+            cout << matrix[sr][i] << " ";
+        }
+        sr++;
+        for (int i = sr; i <= er; i++) {
+            cout << matrix[i][ec] << " ";
+        }
+        ec--;
+        if (sr <= er) {
+            for (int i = ec; i >= sc; i--) {
+                cout << matrix[er][i] << " ";
+            }
+            er--;
+        }
+        if (sc <= ec) {
+            for (int i = er; i >= sr; i--) {
+                cout << matrix[i][sc] << " ";
+            }
+            sc++;
+        }
+    }
     
     return 0;
 }
